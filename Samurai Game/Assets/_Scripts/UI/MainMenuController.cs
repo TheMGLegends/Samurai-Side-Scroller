c2529821a11b362dd5634b68594d89507b4560b8
TheMGLegends <matejovo34@gmail.com>
2025-05-22 17:25:03 +0100

    Highest Wave Logic in Main Menu
    Retrieved from Saving System

--- Samurai Game/Assets/_Scripts/UI/MainMenuController.cs
+++ Samurai Game/Assets/_Scripts/UI/MainMenuController.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private TMPro.TMP_Text highestWaveText;
+
+    private void Start()
+    {
+        // INFO: Load Highest Wave Count from Saving System
+        GameData gameData = SavingSystem.LoadGameData();
+
+        if (highestWaveText != null)
+        {
+            highestWaveText.text = "Highest Wave: " + gameData.highestWave.ToString();
+        }
+    }
+
     public void LoadLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
